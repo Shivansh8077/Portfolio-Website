835e9317fb2d4a1e8edbfde486b0b27f70001f6e
shivanshsharma <contact.shivansh.sharma@gmail.com>
2022-08-03 18:52:35 +0530

    Latest Code On 3rd August 22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+// import './css/Img_change_text.css'
+import './JS/index'
+import Header from './Custom Components/Header';
+import About from './Custom Components/About'
+import Certifications from './Custom Components/Certifications'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  //  Link
+} from "react-router-dom";
+import Projects from './Custom Components/Projects';
+import Footer from './Custom Components/Footer';
+import Resume from './Custom Components/Resume';
+import './css/a.css'
+import 'animate.css';
+
+// import firebase from "firebase";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" >
+            <>
+              <div >
+              <center>
+
+                <p class="animate__animated   animate__lightSpeedInLeft " style={{display:'inline',width:'50%a'}}> Shivansh</p> 
+                <p  class="animate__animated   animate__lightSpeedInRight "  style={{display:'inline'}}>Sharma</p> 
+                
+              </center>
+                
+              </div>
+            </>
+
+          </Route>
+
+          <Route exact path="/about" >
+            <About />
+          </Route>
+
+          <Route exact path="/certifications" >
+            <Certifications />
+          </Route>
+
+          <Route exact path="/projects" >
+            <Projects />
+          </Route>
+
+
+          <Route exact path="/resume" >
+            <Resume />
+          </Route>
+
+
+        </Switch>
+
+        <Footer />
+      </Router>
+    </>
   );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
+
+// firebase.database().ref('Personal Portfolio/' + 'Projects').on('value', function (snapshot) {
+//   console.log(snapshot.val()['GLAU Aide Memoire']);
+// })
+// import firebase from 'firebase';
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCHj253SLY7_RisdfWtv0zJLqEvB2i2Guw",
+//   authDomain: "fir-demo-d760f.firebaseapp.com",
+//   databaseURL: "https://fir-demo-d760f-default-rtdb.firebaseio.com",
+//   projectId: "fir-demo-d760f",
+//   storageBucket: "fir-demo-d760f.appspot.com",
+//   messagingSenderId: "835102837793",
+//   appId: "1:835102837793:web:5014939774043806a02760",
+//   measurementId: "G-DYC8G5CB9R"
+// };
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,7 +26,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
